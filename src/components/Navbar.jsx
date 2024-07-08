3fa425b800e2101189dcf364c149bf8470783f38
Anubrata-01 <anubratachanda36@gmail.com>
2024-07-08 10:10:50 +0530

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,40 +15,66 @@ const Navbar = () => {
   return (
     <div className="w-full p-4 ">
       <div className="flex justify-between items-center w-[95%] ml-4 relative">
-        <div className="ml-[6%] md:ml-[5%] cursor-pointer ">
-          <span className=" text-orange-950 box-border">
-          <img src={titleLogo} alt="titleLogo" className=" w-[40%] md:w-[200px] p-0"/>
-          </span>
-        </div>
+      <div className="ml-[6%] md:ml-[5%] cursor-pointer">
+  <picture className="text-orange-950 box-border">
+    <source
+      srcSet="src/assets/myimg.gif"
+      media="(min-width: 1000px)"
+      width="1000"
+      height="400"
+    />
+    <img 
+      src={titleLogo} 
+      alt="Title Logo" 
+      className="w-[40%] md:w-[200px] p-0 bg-black" 
+    />
+  </picture>
+</div>
+
 
         {/* Hidden on large screens, visible on medium and smaller screens */}
-        <button className="lg:hidden bg-violet-600 rounded-md mr-[5%]" onClick={handleMenu}>
-          {!displayNavItems ? <AlignJustify /> : <X />}
-        </button>
+        <button
+        className="lg:hidden bg-violet-600 rounded-md mr-[5%]"
+        onClick={handleMenu}
+        aria-label={displayNavItems ? "Close menu" : "Open menu"}
+      >
+        {!displayNavItems ? <AlignJustify /> : <X />}
+      </button>
 
         {/* Nav items for large screens */}
         <div className="hidden lg:flex gap-4 md:mr-[5%]">
-          <span className=" p-2 text-orange-500 uppercase cursor-pointer">
-            Home
-          </span>
-          <span className=" p-2 text-orange-500 uppercase">About</span>
-          <span className=" p-2 text-orange-500 uppercase">Project</span>
-          <span className=" p-2 text-orange-500 uppercase">Contact</span>
-        </div>
+  <a href="#home" className="p-2 text-orange-500 uppercase cursor-pointer" aria-label="Home">
+    Home
+  </a>
+  <a href="#about" className="p-2 text-orange-500 uppercase" aria-label="About">
+    About
+  </a>
+  <a href="#project" className="p-2 text-orange-500 uppercase" aria-label="Project">
+    Project
+  </a>
+  <a href="#contact" className="p-2 text-orange-500 uppercase" aria-label="Contact">
+    Contact
+  </a>
+</div>
 
-        {/* Nav items for medium and smaller screens */}
-        {displayNavItems && (
-          <div className="lg:hidden w-[400px] md:w-[300px]  mt-72 p-4  flex flex-col absolute -right-4 gap-3 bg-emerald-800">
-            <span className="navmenu p-2 text-orange-500 uppercase">Home</span>
-            <span className="navmenu p-2 text-orange-500 uppercase">About</span>
-            <span className="navmenu p-2 text-orange-500 uppercase">
-              Project
-            </span>
-            <span className="navmenu p-2 text-orange-500 uppercase">
-              Contact
-            </span>
-          </div>
-        )}
+{/* Nav items for medium and smaller screens */}
+{displayNavItems && (
+  <div className="lg:hidden w-[400px] md:w-[300px] mt-72 p-4 flex flex-col absolute -right-4 gap-3 bg-emerald-800" role="menu">
+    <a href="#home" className="navmenu p-2 text-orange-500 uppercase" aria-label="Home" role="menuitem">
+      Home
+    </a>
+    <a href="#about" className="navmenu p-2 text-orange-500 uppercase" aria-label="About" role="menuitem">
+      About
+    </a>
+    <a href="#project" className="navmenu p-2 text-orange-500 uppercase" aria-label="Project" role="menuitem">
+      Project
+    </a>
+    <a href="#contact" className="navmenu p-2 text-orange-500 uppercase" aria-label="Contact" role="menuitem">
+      Contact
+    </a>
+  </div>
+)}
+
       </div>
     </div>
   );
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,5 +1,6 @@
 // import React from 'react'
 
+import About from "../components/About"
 import Hero from "../components/Hero"
 import Navbar from "../components/Navbar"
 
@@ -8,6 +9,7 @@ const Home = () => {
     <div>
         <Navbar/>
         <Hero/>
+        <About/>
     </div>
   )
 }
