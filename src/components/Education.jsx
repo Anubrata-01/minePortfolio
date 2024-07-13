5570bec3a47bb018d0d419a3c02403f7dd5501d4
Anubrata-01 <anubratachanda36@gmail.com>
2024-07-13 17:54:41 +0530

    created education and skilss component

---
+++ src/components/Education.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+import MyEducationalDetails from "../details/data";
+
+const Education = () => {
+  return (
+    <section className="education-section">
+      {MyEducationalDetails.map((details, i) => (
+        <div className="education-card" key={i}>
+          <h1 className="degree">{details?.Degree}</h1>
+          <h3 className="college">{details?.College}</h3>
+          <p className="duration">{details?.From} - {details?.Year}  <span className=" ml-5">Per: {details?.Percentage}</span></p>
+        </div>
+      ))}
+    </section>
+  );
+}
+
+export default Education;
---
+++ src/components/Skills.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+const Skills = () => {
+  return (
+    <div>Skills Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, sit!</div>
+  )
+}
+
+export default Skills
