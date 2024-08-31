9422b61649fd13719f0464d79a579b94337af556
Anubrata-01 <anubratachanda36@gmail.com>
2024-08-31 17:55:45 +0530

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="pt-10 flex justify-center">
       <div className="w-full max-w-7xl flex flex-col md:flex-row items-center md:items-start justify-between">
-        <div className="w-full md:w-5/12 p-3 text-white md:mt-20">
+        <div className="w-full md:w-5/12 p-3 text-white md:mt-20 ml-[2%] md:ml-[6%]">
           <h1 className="text-5xl md:text-7xl text-center md:text-left text-[#ff004f] font-semibold">
             Welcome!
           </h1>
@@ -23,7 +23,7 @@ const Hero = () => {
             />
             <div className="mt-16 flex justify-center md:justify-start">
               <a 
-                href="public\AnubrataResumeTemplate.docx" 
+                href="/AnubrataResumeTemplate.pdf" 
                 download 
                 className=" bg-cyan-500 text-white p-2 text-lg rounded-md shadow-md hover:bg-[#e7a4b6] transition-all duration-300"
               >
